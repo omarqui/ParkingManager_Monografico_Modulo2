11a82d5df47e6df833f1ed27dfe1162596e8efd3
Edwin <70494680+EdwinMNM@users.noreply.github.com>
2020-09-03 00:15:01 -0400

    Cambio: capaEntidades, se agregaron las entidades faltantes.

---
+++ Entidades/ConfiguracionEN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class ConfiguracionEN
+    {
+        public string NombreEmpresa { get; set; }
+        public string DireccionEmpresa { get; set; }
+        public string TelefonoEmpresa { get; set; }
+        public decimal PrecioPorHora { get; set; }
+        public int CantidadParqueos { get; set; }
+    }
+}
