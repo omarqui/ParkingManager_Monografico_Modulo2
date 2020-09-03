266ee15dca8fad56b1c8e5f576be00d63f49d53d
Edwin <70494680+EdwinMNM@users.noreply.github.com>
2020-09-03 00:00:07 -0400

    Creacion de las entidades en la capa de entidades

---
+++ Entidades/TurnoEN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class TurnoEN
+    {
+        public int IdTurno { get; set; }
+        public int IdEmpleado { get; set; }
+        public DateTime  FechaApertura { get; set; }
+        public DateTime FechaCierre { get; set; }
+        public float MontoApertura { get; set; }
+        public float MontoCobrado { get; set; }
+        public float MontoEntregado { get; set; }
+        public float MontoDiferencia { get; set; }
+    }
+}
