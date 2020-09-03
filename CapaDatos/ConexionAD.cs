7ffff2fbbaa72a44676d9c52f2b8e57a706edc3e
edickgcruz <47405682+edickgcruz@users.noreply.github.com>
2020-09-02 23:07:43 -0400

    Crear Clase de Conexion a base de datos

--- CapaDatos/ConexionAD.cs
+++ CapaDatos/ConexionAD.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,50 @@ namespace CapaDatos
 {
     class ConexionAD
     {
+        //ASI ES COMO EL PROFESOR LO UTILIZA
         public static string ObetenerConexion()
         {
             return @"Data Source=MRCODE\MRCODE2017;Initial Catalog=DB_ESTACIONAMIENTO; Integrated Security=True";
         }
+
+
+        //COMO YO LO USO 
+        //public static string cadenaConexion { get; set; }
+        ///// <summary>
+        ///// Este metodo busca el archivo donde esta la conexion y la crear para poder ser utilizada
+        ///// </summary>
+        ///// <returns>Retorna un objeto de tipo conexion para ser usado por la app en los procesos donde sea solicitado</returns>
+        //public static SqlConnection Abrir_Conexion()
+        //{
+        //    SqlConnection Conectar = new SqlConnection();
+
+        //    StreamReader leerConexion = new StreamReader(@"C:\conexion_monografico\conexion.txt");
+        //    cadenaConexion = leerConexion.ReadLine();
+
+        //    Conectar = new SqlConnection(cadenaConexion);
+        //    //Conectar = new SqlConnection("server=MRCODE\\MRCODE2017; database=BD_VENTAS_OFICIAL; user = monografico; password = clavesql");
+
+        //    Conectar.Open();
+        //    return Conectar;
+        //}
+
+
+        //PROTOTIPO PARA QUE FUNCIONE IGUAL QUE COMO EL PROFE LO USA PERO MAS DINAMICO Y NO HAYA QUE CAMBIAR CONEXIONES 
+
+        //public static string cadenaConexion { get; set; }
+        ///// <summary>
+        ///// Este metodo busca el archivo donde esta la conexion y la crear para poder ser utilizada
+        ///// </summary>
+        ///// <returns>Retorna un objeto de tipo conexion para ser usado por la app en los procesos donde sea solicitado</returns>
+        //public static string Abrir_Conexion()
+        //{
+        //    StreamReader leerConexion = new StreamReader(@"C:\conexion_monografico\conexion.txt");
+        //    cadenaConexion = leerConexion.ReadLine();
+
+
+        //    return cadenaConexion;
+        //}
+
+
     }
 }
--- Presentacion/Properties/Settings.Designer.cs
+++ Presentacion/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CapaPresentacion.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,17 @@ namespace CapaPresentacion.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MRCODE\\MRCODE2017;Initial Catalog=DB_ESTACIONAMIENTO;Integrated Secur" +
+            "ity=True")]
+        public string ConexionDB {
+            get {
+                return ((string)(this["ConexionDB"]));
             }
         }
     }
