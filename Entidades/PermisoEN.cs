6cb7885b38f750eb94d146899c200ef778f83ac7
edickgcruz <47405682+edickgcruz@users.noreply.github.com>
2020-09-03 00:06:17 -0400

    Creando entidades Uso de parqueo, Permiso, Configuracion

---
+++ Entidades/PermisoEN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class PermisoEN
+    {
+        public int IdEmpleado { get; set; }
+        public int IdFormulario { get; set; }
+        public bool TieneAcceso { get; set; }
+    }
+}
---
+++ Entidades/UsoDeParqueoEN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class UsoDeParqueoEN
+    {
+        public int IdUso { get; set; }
+        public int IdTurno { get; set; }
+        public DateTime FechaEntrada { get; set; }
+        public DateTime FechaSalida { get; set; }
+        public decimal TiempoUso { get; set; }
+        public decimal PrecioPorMinuto { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal Total { get; set; }
+        public decimal MontoPagado { get; set; }
+        public decimal Devuelta { get; set; }
+    }
+}
