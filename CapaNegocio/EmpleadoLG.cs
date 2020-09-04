04793fbf8b85102ad75c5d34d1bf27efb21e1e0d
edickgcruz <47405682+edickgcruz@users.noreply.github.com>
2020-09-03 23:55:14 -0400

    Creacion de los metodos en cama logica de Empleado

--- CapaNegocio/EmpleadoLG.cs
+++ CapaNegocio/EmpleadoLG.cs
@@ -16,5 +16,29 @@ namespace CapaNegocio
 
             return empleadoAD.BuscarPorID(id);
         }
+
+        public Empleado BuscarPorUsuario(string usuario)
+        {
+            IEmpleadoAD empleadoAD = null;
+
+            return empleadoAD.BuscarPorUsuario(usuario);
+
+        }
+
+        public bool EsUsuarioValido(string usuario, string clave)
+        {
+            IEmpleadoAD empleado = null;
+
+            return empleado.EsUsuarioValido(usuario, clave);
+        }
+
+        //NOTA: considero que el metodo Guardar en la capa de datos deberia devolver un true o algo que le haga saber
+        // a la capa de presentacion que se guardo con exito. Puede ser en esta misma capa tambien que devuelva este valor.
+        public void GuardarEmpleado(Empleado empleado)
+        {
+            IEmpleadoAD empleadoAD = null;
+
+            empleadoAD.Guardar(empleado);
+        }
     }
 }
