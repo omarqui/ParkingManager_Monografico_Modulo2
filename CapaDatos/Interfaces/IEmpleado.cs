4fd0f40421c0abd84abf732d97e4e724536060b6
Omarqui Aybar <omarqui1996@gmail.com>
2020-09-03 22:28:47 -0400

    agrega interfaz de usuario

---
+++ CapaDatos/Interfaces/IEmpleado.cs
@@ -0,0 +1,20 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Interfaces
+{
+    public interface IEmpleado
+    {
+        void Guardar(Empleado empleado);
+
+        Empleado BuscarPorID(int id);
+
+        Empleado BuscarPorUsuario(string usuario);
+
+        bool EsUsuarioValido(string usuario, string clave);
+    }
+}
