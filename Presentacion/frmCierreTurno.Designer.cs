a22c3f12849e7f9ad10c36b0d41bacc05af2a4d7
Omarqui Aybar <omarqui1996@gmail.com>
2020-09-26 15:30:56 -0400

    Crea copia de form Turno a Cierre turno

---
+++ Presentacion/frmCierreTurno.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmCierreTurno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTurno));
+            this.lblCodigoTurno = new System.Windows.Forms.Label();
+            this.lblCodigoEmpleadoTurno = new System.Windows.Forms.Label();
+            this.lblFechaApertura = new System.Windows.Forms.Label();
+            this.lblFechaCierre = new System.Windows.Forms.Label();
+            this.lblMontoApertura = new System.Windows.Forms.Label();
+            this.lblMontoCobrado = new System.Windows.Forms.Label();
+            this.lblMontoEntregado = new System.Windows.Forms.Label();
+            this.lblMontoDiferencia = new System.Windows.Forms.Label();
+            this.txtCodigoTurno = new System.Windows.Forms.TextBox();
+            this.txtCodigoEmpleadoTurno = new System.Windows.Forms.TextBox();
+            this.txtMontoApertura = new System.Windows.Forms.TextBox();
+            this.txtMontoCobrado = new System.Windows.Forms.TextBox();
+            this.txtMontoEntregado = new System.Windows.Forms.TextBox();
+            this.txtMontoDiferencia = new System.Windows.Forms.TextBox();
+            this.dtpFechaApertura = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaCierre = new System.Windows.Forms.DateTimePicker();
+            this.lblFrmTurno = new System.Windows.Forms.Label();
+            this.lblNombreEmpleado = new System.Windows.Forms.Label();
+            this.txtNombreEmpleado = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btnRegistrarTurno = new System.Windows.Forms.Button();
+            this.btnSalirTurno = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigoTurno
+            // 
+            this.lblCodigoTurno.AutoSize = true;
+            this.lblCodigoTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblCodigoTurno.ForeColor = System.Drawing.Color.White;
+            this.lblCodigoTurno.Location = new System.Drawing.Point(19, 130);
+            this.lblCodigoTurno.Name = "lblCodigoTurno";
+            this.lblCodigoTurno.Size = new System.Drawing.Size(88, 25);
+            this.lblCodigoTurno.TabIndex = 0;
+            this.lblCodigoTurno.Text = "ID Turno";
+            // 
+            // lblCodigoEmpleadoTurno
+            // 
+            this.lblCodigoEmpleadoTurno.AutoSize = true;
+            this.lblCodigoEmpleadoTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblCodigoEmpleadoTurno.ForeColor = System.Drawing.Color.White;
+            this.lblCodigoEmpleadoTurno.Location = new System.Drawing.Point(19, 187);
+            this.lblCodigoEmpleadoTurno.Name = "lblCodigoEmpleadoTurno";
+            this.lblCodigoEmpleadoTurno.Size = new System.Drawing.Size(124, 25);
+            this.lblCodigoEmpleadoTurno.TabIndex = 1;
+            this.lblCodigoEmpleadoTurno.Text = "ID Empleado";
+            // 
+            // lblFechaApertura
+            // 
+            this.lblFechaApertura.AutoSize = true;
+            this.lblFechaApertura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblFechaApertura.ForeColor = System.Drawing.Color.White;
+            this.lblFechaApertura.Location = new System.Drawing.Point(19, 301);
+            this.lblFechaApertura.Name = "lblFechaApertura";
+            this.lblFechaApertura.Size = new System.Drawing.Size(147, 25);
+            this.lblFechaApertura.TabIndex = 2;
+            this.lblFechaApertura.Text = "Fecha Apertura";
+            // 
+            // lblFechaCierre
+            // 
+            this.lblFechaCierre.AutoSize = true;
+            this.lblFechaCierre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblFechaCierre.ForeColor = System.Drawing.Color.White;
+            this.lblFechaCierre.Location = new System.Drawing.Point(19, 358);
+            this.lblFechaCierre.Name = "lblFechaCierre";
+            this.lblFechaCierre.Size = new System.Drawing.Size(125, 25);
+            this.lblFechaCierre.TabIndex = 3;
+            this.lblFechaCierre.Text = "Fecha Cierre";
+            // 
+            // lblMontoApertura
+            // 
+            this.lblMontoApertura.AutoSize = true;
+            this.lblMontoApertura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblMontoApertura.ForeColor = System.Drawing.Color.White;
+            this.lblMontoApertura.Location = new System.Drawing.Point(419, 130);
+            this.lblMontoApertura.Name = "lblMontoApertura";
+            this.lblMontoApertura.Size = new System.Drawing.Size(147, 25);
+            this.lblMontoApertura.TabIndex = 4;
+            this.lblMontoApertura.Text = "Monto Apertura";
+            // 
+            // lblMontoCobrado
+            // 
+            this.lblMontoCobrado.AutoSize = true;
+            this.lblMontoCobrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblMontoCobrado.ForeColor = System.Drawing.Color.White;
+            this.lblMontoCobrado.Location = new System.Drawing.Point(419, 182);
+            this.lblMontoCobrado.Name = "lblMontoCobrado";
+            this.lblMontoCobrado.Size = new System.Drawing.Size(148, 25);
+            this.lblMontoCobrado.TabIndex = 5;
+            this.lblMontoCobrado.Text = "Monto Cobrado";
+            // 
+            // lblMontoEntregado
+            // 
+            this.lblMontoEntregado.AutoSize = true;
+            this.lblMontoEntregado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblMontoEntregado.ForeColor = System.Drawing.Color.White;
+            this.lblMontoEntregado.Location = new System.Drawing.Point(419, 234);
+            this.lblMontoEntregado.Name = "lblMontoEntregado";
+            this.lblMontoEntregado.Size = new System.Drawing.Size(162, 25);
+            this.lblMontoEntregado.TabIndex = 6;
+            this.lblMontoEntregado.Text = "Monto Entregado";
+            // 
+            // lblMontoDiferencia
+            // 
+            this.lblMontoDiferencia.AutoSize = true;
+            this.lblMontoDiferencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblMontoDiferencia.ForeColor = System.Drawing.Color.White;
+            this.lblMontoDiferencia.Location = new System.Drawing.Point(424, 286);
+            this.lblMontoDiferencia.Name = "lblMontoDiferencia";
+            this.lblMontoDiferencia.Size = new System.Drawing.Size(159, 25);
+            this.lblMontoDiferencia.TabIndex = 7;
+            this.lblMontoDiferencia.Text = "Monto Diferencia";
+            // 
+            // txtCodigoTurno
+            // 
+            this.txtCodigoTurno.Location = new System.Drawing.Point(203, 134);
+            this.txtCodigoTurno.Name = "txtCodigoTurno";
+            this.txtCodigoTurno.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigoTurno.TabIndex = 8;
+            // 
+            // txtCodigoEmpleadoTurno
+            // 
+            this.txtCodigoEmpleadoTurno.Location = new System.Drawing.Point(203, 186);
+            this.txtCodigoEmpleadoTurno.Name = "txtCodigoEmpleadoTurno";
+            this.txtCodigoEmpleadoTurno.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigoEmpleadoTurno.TabIndex = 9;
+            // 
+            // txtMontoApertura
+            // 
+            this.txtMontoApertura.Location = new System.Drawing.Point(598, 134);
+            this.txtMontoApertura.Name = "txtMontoApertura";
+            this.txtMontoApertura.Size = new System.Drawing.Size(200, 22);
+            this.txtMontoApertura.TabIndex = 10;
+            // 
+            // txtMontoCobrado
+            // 
+            this.txtMontoCobrado.Location = new System.Drawing.Point(598, 186);
+            this.txtMontoCobrado.Name = "txtMontoCobrado";
+            this.txtMontoCobrado.Size = new System.Drawing.Size(200, 22);
+            this.txtMontoCobrado.TabIndex = 11;
+            // 
+            // txtMontoEntregado
+            // 
+            this.txtMontoEntregado.Location = new System.Drawing.Point(598, 238);
+            this.txtMontoEntregado.Name = "txtMontoEntregado";
+            this.txtMontoEntregado.Size = new System.Drawing.Size(200, 22);
+            this.txtMontoEntregado.TabIndex = 12;
+            // 
+            // txtMontoDiferencia
+            // 
+            this.txtMontoDiferencia.Location = new System.Drawing.Point(598, 290);
+            this.txtMontoDiferencia.Name = "txtMontoDiferencia";
+            this.txtMontoDiferencia.Size = new System.Drawing.Size(200, 22);
+            this.txtMontoDiferencia.TabIndex = 13;
+            // 
+            // dtpFechaApertura
+            // 
+            this.dtpFechaApertura.Enabled = false;
+            this.dtpFechaApertura.Location = new System.Drawing.Point(203, 310);
+            this.dtpFechaApertura.Name = "dtpFechaApertura";
+            this.dtpFechaApertura.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaApertura.TabIndex = 14;
+            // 
+            // dtpFechaCierre
+            // 
+            this.dtpFechaCierre.Enabled = false;
+            this.dtpFechaCierre.Location = new System.Drawing.Point(203, 361);
+            this.dtpFechaCierre.Name = "dtpFechaCierre";
+            this.dtpFechaCierre.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaCierre.TabIndex = 15;
+            // 
+            // lblFrmTurno
+            // 
+            this.lblFrmTurno.AutoSize = true;
+            this.lblFrmTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblFrmTurno.ForeColor = System.Drawing.Color.White;
+            this.lblFrmTurno.Location = new System.Drawing.Point(295, 36);
+            this.lblFrmTurno.Name = "lblFrmTurno";
+            this.lblFrmTurno.Size = new System.Drawing.Size(295, 46);
+            this.lblFrmTurno.TabIndex = 26;
+            this.lblFrmTurno.Text = " Registro Turno";
+            this.lblFrmTurno.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNombreEmpleado
+            // 
+            this.lblNombreEmpleado.AutoSize = true;
+            this.lblNombreEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblNombreEmpleado.ForeColor = System.Drawing.Color.White;
+            this.lblNombreEmpleado.Location = new System.Drawing.Point(20, 244);
+            this.lblNombreEmpleado.Name = "lblNombreEmpleado";
+            this.lblNombreEmpleado.Size = new System.Drawing.Size(174, 25);
+            this.lblNombreEmpleado.TabIndex = 28;
+            this.lblNombreEmpleado.Text = "Nombre Empleado";
+            // 
+            // txtNombreEmpleado
+            // 
+            this.txtNombreEmpleado.Location = new System.Drawing.Point(203, 248);
+            this.txtNombreEmpleado.Name = "txtNombreEmpleado";
+            this.txtNombreEmpleado.Size = new System.Drawing.Size(200, 22);
+            this.txtNombreEmpleado.TabIndex = 29;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.registrar_grand;
+            this.pictureBox1.Location = new System.Drawing.Point(240, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::CapaPresentacion.Properties.Resources.logo_Parking1;
+            this.pictureBox4.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(41, 34);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 25;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btnRegistrarTurno
+            // 
+            this.btnRegistrarTurno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(36)))), ((int)(((byte)(85)))));
+            this.btnRegistrarTurno.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegistrarTurno.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnRegistrarTurno.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrarTurno.Image = global::CapaPresentacion.Properties.Resources.registrar;
+            this.btnRegistrarTurno.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistrarTurno.Location = new System.Drawing.Point(424, 345);
+            this.btnRegistrarTurno.Name = "btnRegistrarTurno";
+            this.btnRegistrarTurno.Size = new System.Drawing.Size(166, 38);
+            this.btnRegistrarTurno.TabIndex = 17;
+            this.btnRegistrarTurno.Text = "Registrar Turno";
+            this.btnRegistrarTurno.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnRegistrarTurno.UseVisualStyleBackColor = false;
+            this.btnRegistrarTurno.Click += new System.EventHandler(this.btnRegistrarTurno_Click);
+            // 
+            // btnSalirTurno
+            // 
+            this.btnSalirTurno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(36)))), ((int)(((byte)(85)))));
+            this.btnSalirTurno.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalirTurno.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnSalirTurno.ForeColor = System.Drawing.Color.White;
+            this.btnSalirTurno.Image = global::CapaPresentacion.Properties.Resources.Exit_64px;
+            this.btnSalirTurno.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalirTurno.Location = new System.Drawing.Point(690, 345);
+            this.btnSalirTurno.Name = "btnSalirTurno";
+            this.btnSalirTurno.Size = new System.Drawing.Size(108, 38);
+            this.btnSalirTurno.TabIndex = 16;
+            this.btnSalirTurno.Text = "Salir";
+            this.btnSalirTurno.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSalirTurno.UseVisualStyleBackColor = false;
+            this.btnSalirTurno.Click += new System.EventHandler(this.btnSalirTurno_Click);
+            // 
+            // frmTurno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(31)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(825, 459);
+            this.Controls.Add(this.txtNombreEmpleado);
+            this.Controls.Add(this.lblNombreEmpleado);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblFrmTurno);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btnRegistrarTurno);
+            this.Controls.Add(this.btnSalirTurno);
+            this.Controls.Add(this.dtpFechaCierre);
+            this.Controls.Add(this.dtpFechaApertura);
+            this.Controls.Add(this.txtMontoDiferencia);
+            this.Controls.Add(this.txtMontoEntregado);
+            this.Controls.Add(this.txtMontoCobrado);
+            this.Controls.Add(this.txtMontoApertura);
+            this.Controls.Add(this.txtCodigoEmpleadoTurno);
+            this.Controls.Add(this.txtCodigoTurno);
+            this.Controls.Add(this.lblMontoDiferencia);
+            this.Controls.Add(this.lblMontoEntregado);
+            this.Controls.Add(this.lblMontoCobrado);
+            this.Controls.Add(this.lblMontoApertura);
+            this.Controls.Add(this.lblFechaCierre);
+            this.Controls.Add(this.lblFechaApertura);
+            this.Controls.Add(this.lblCodigoEmpleadoTurno);
+            this.Controls.Add(this.lblCodigoTurno);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmTurno";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmTurno";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigoTurno;
+        private System.Windows.Forms.Label lblCodigoEmpleadoTurno;
+        private System.Windows.Forms.Label lblFechaApertura;
+        private System.Windows.Forms.Label lblFechaCierre;
+        private System.Windows.Forms.Label lblMontoApertura;
+        private System.Windows.Forms.Label lblMontoCobrado;
+        private System.Windows.Forms.Label lblMontoEntregado;
+        private System.Windows.Forms.Label lblMontoDiferencia;
+        private System.Windows.Forms.TextBox txtCodigoTurno;
+        private System.Windows.Forms.TextBox txtCodigoEmpleadoTurno;
+        private System.Windows.Forms.TextBox txtMontoApertura;
+        private System.Windows.Forms.TextBox txtMontoCobrado;
+        private System.Windows.Forms.TextBox txtMontoEntregado;
+        private System.Windows.Forms.TextBox txtMontoDiferencia;
+        private System.Windows.Forms.DateTimePicker dtpFechaApertura;
+        private System.Windows.Forms.DateTimePicker dtpFechaCierre;
+        private System.Windows.Forms.Button btnSalirTurno;
+        private System.Windows.Forms.Button btnRegistrarTurno;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblFrmTurno;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblNombreEmpleado;
+        private System.Windows.Forms.TextBox txtNombreEmpleado;
+    }
+}
