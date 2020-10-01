2567ff034f37558602696bb7399d4d0b6bd765b1
Omarqui Aybar <omarqui1996@gmail.com>
2020-09-30 21:42:13 -0400

    Crear clase reportero
    Mejora reporte de turno

--- Presentacion/Reportes/FrmReportViwer.cs
+++ Presentacion/Reportes/FrmReportViwer.cs
@@ -14,24 +14,42 @@ namespace CapaPresentacion.Reportes
 {
     public partial class FrmReportViwer : Form
     {
+        private string _nombreReporte;
+        Dictionary<string, DataTable> _fuentesDatos;
+
         public FrmReportViwer()
         {
             InitializeComponent();
+            _nombreReporte = "RptTurno";
+            _fuentesDatos = new Dictionary<string, DataTable>
+            {
+                { "Configuracion", ConfiguracionLG.BuscarConfiguracion() },
+                { "Turno", TurnoLG.BuscarTurnoImpresion(1) }
+            };
+        }
+
+        public FrmReportViwer(
+            string nombreReporte,
+            Dictionary<string, DataTable> fuentesDatos)
+        {
+            InitializeComponent();
+            _nombreReporte = nombreReporte;
+            _fuentesDatos = fuentesDatos;
         }
 
         private void FrmReportViwer_Load(object sender, EventArgs e)
         {
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "CapaPresentacion.Reportes.RptTurno.rdlc";
+            reportViewer1.LocalReport.ReportEmbeddedResource = $"CapaPresentacion.Reportes.{_nombreReporte}.rdlc";
 
-            ReportDataSource configuracion = new ReportDataSource("Configuracion", ConfiguracionLG.BuscarConfiguracion());
-            ReportDataSource cobro = new ReportDataSource("Turno", TurnoLG.BuscarTurnoImpresion(1));
             reportViewer1.LocalReport.DataSources.Clear();
-            reportViewer1.LocalReport.DataSources.Add(configuracion);
-            reportViewer1.LocalReport.DataSources.Add(cobro);
+            foreach (var fuente in _fuentesDatos)
+            {
+                ReportDataSource reportData = new ReportDataSource(fuente.Key, fuente.Value);
+                reportViewer1.LocalReport.DataSources.Add(reportData);
+            }
 
-            reportViewer1.LocalReport.Refresh();
-
-            this.reportViewer1.RefreshReport();
+            //reportViewer1.LocalReport.Refresh();
+            reportViewer1.RefreshReport();
         }
     }
 }
---
+++ Presentacion/Reportes/Reportero.cs
@@ -0,0 +1,22 @@
+﻿using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaPresentacion.Reportes
+{
+    public class Reportero
+    {
+        public static void Imprimir(
+            string nombreReporte,
+            Dictionary<string, DataTable> fuentesDatos)
+        {
+            fuentesDatos.Add("Configuracion", ConfiguracionLG.BuscarConfiguracion());
+            FrmReportViwer reportViwer = new FrmReportViwer(nombreReporte, fuentesDatos);
+            reportViwer.ShowDialog();
+        }
+    }
+}
