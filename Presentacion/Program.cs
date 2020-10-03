a6a9cd9688ab8c06b604eda81cd2a5178c26b850
Omarqui Aybar <omarqui1996@gmail.com>
2020-10-03 16:25:18 -0400

    Ultimos retoques

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -17,7 +17,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmConfiguracion());
+            Application.Run(new frmLogin());
         }
     }
 }
